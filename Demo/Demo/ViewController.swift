cabea2f0385e17cb705c109a477c655ba00f6a16
Andreas <andreas@flatcircle.io>
2020-11-16 11:39:16 +0200

    Merge commit 'fcc93c8d925f32073a8e94866d087344976c34bd'
    # Conflicts:
    #	BTNavigationDropdownMenu.podspec
    #	Demo/Demo.xcodeproj/project.pbxproj
    #	Demo/Demo.xcodeproj/xcshareddata/xcschemes/Demo.xcscheme
    #	Demo/Demo/AppDelegate.swift
    #	Demo/Demo/ViewController.swift
    #	Source/BTNavigationDropdownMenu.swift
    #	Source/Internal/BTConfiguration.swift
    #	Source/Internal/BTTableView.swift

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -13,57 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var selectedCellLabel: UILabel!
     var menuView: BTNavigationDropdownMenu!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadImageExample()
-    }
-    
-    func loadImageExample() {
-        let images = [#imageLiteral(resourceName: "logo-timeslive"), #imageLiteral(resourceName: "ic-logo-sunday-times"), #imageLiteral(resourceName: "logo_e_edition")]
-        self.selectedCellLabel.text = "??????"
-        self.navigationController?.navigationBar.isTranslucent = false
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green: 180/255.0, blue: 220/255.0, alpha: 1.0)
-        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        
-        menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, config: BTConfiguration(), title: #imageLiteral(resourceName: "logo-timeslive"), images: images)
-        
-        menuView.imageMode = true
-        menuView.dynamicSelection = [true, true, false]
-        menuView.dynamicBackgrounds = [.brown, .white, .white]
-        menuView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 5))
-        menuView.tableFooterView.backgroundColor = .red
-        menuView.cellImageHeight = 20
-        menuView.cellHeight = 50
-        menuView.arrowPadding = 15
-        menuView.animationDuration = 0.5
-        menuView.maskBackgroundColor = UIColor.black
-        menuView.maskBackgroundOpacity = 0.3
-        menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> Void in
-            print("Did select item at index: \(indexPath)")
-        }
-        
-        self.navigationItem.titleView = menuView
-    }
-    
-    func loadTextExample() {
         let items = ["Most Popular", "Latest", "Trending", "Nearest", "Top Picks"]
         self.selectedCellLabel.text = items.first
         self.navigationController?.navigationBar.isTranslucent = false
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green: 180/255.0, blue: 220/255.0, alpha: 1.0)
-        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        
-        
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green:180/255.0, blue:220/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+
+        // "Old" version
+        // menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, title: "Dropdown Menu", items: items)
+
         menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, title: BTTitle.index(2), items: items)
 
-        
+        // Another way to initialize:
+        // menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, title: BTTitle.title("Dropdown Menu"), items: items)
+
         menuView.cellHeight = 50
         menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
-        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green: 160.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
         menuView.shouldKeepSelectedCellColor = true
         menuView.cellTextLabelColor = UIColor.white
         menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
-        menuView.cellTextLabelAlignment = .left
+        menuView.cellTextLabelAlignment = .left // .Center // .Right // .Left
         menuView.arrowPadding = 15
         menuView.animationDuration = 0.5
         menuView.maskBackgroundColor = UIColor.black
